be81229c8b12052804285c5539c9f7a3de0498fe
moklick <info@moritzklack.com>
2023-11-09 16:03:30 +0100

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# Vite Svelte Flow Template
+
+This template creates a very basic [Svelte Flow](https://svelteflow.dev) app with [Vite](https://vite.dev).
+
+## Get it!
+
+```sh
+npx degit xyflow/vite-svelte-flow-template app-name
+```
+
+## Installation
+
+```sh
+npm run install
+```
+
+## Development
+
+```sh
+npm run dev
+```
+
+## Build
+
+```sh
+npm run build
+```
---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { writable } from 'svelte/store';
+  import {
+    SvelteFlow,
+    Controls,
+    Background,
+    MiniMap,
+    type Node,
+    type Edge,
+  } from '@xyflow/svelte';
+
+  import '@xyflow/svelte/dist/style.css';
+
+  const nodes = writable<Node[]>([
+    {
+      id: '1',
+      data: { label: 'Hello' },
+      position: { x: 0, y: 0 },
+    },
+    {
+      id: '2',
+      data: { label: 'World' },
+      position: { x: 150, y: 150 },
+    },
+  ]);
+
+  const edges = writable<Edge[]>([
+    {
+      id: '1-2',
+      source: '1',
+      target: '2',
+    },
+  ]);
+</script>
+
+<div style:height="100vh">
+  <SvelteFlow {nodes} {edges} fitView>
+    <Controls />
+    <Background />
+    <MiniMap />
+  </SvelteFlow>
+</div>
