e560080605707a5eeb27a56a0c4fd0cb2f547d99
peterkogo <peter@xyflow.com>
2025-04-16 18:52:31 +0200

    update vite template

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import App from './App.svelte';
+import { mount } from "svelte";
 
-import './app.css';
+import App from "./App.svelte";
 
-const app = new App({
-  target: document.getElementById('app')!,
+import "./app.css";
+
+// const app = new App({
+//   target: document.getElementById('app')!,
+// });
+
+const app = mount(App, {
+  target: document.getElementById("app")!,
 });
 
 export default app;
