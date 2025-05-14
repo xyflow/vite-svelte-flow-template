e36d13b5c0e2a2fd5ce682565bd631284b8b9a29
peterkogo <peter@xyflow.com>
2025-05-14 12:39:54 +0200

    remove comment

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import App from "./App.svelte";
 
 import "./app.css";
 
-// const app = new App({
-//   target: document.getElementById('app')!,
-// });
-
 const app = mount(App, {
   target: document.getElementById("app")!,
 });
